4e5e391855551391229f22ac9b4dd8299f3bdda5
Ejrud <den.maltsev1@gmail.com>
2022-07-30 20:21:23 +0400

    Tutorial 90%

---
+++ Assets/Scripts/GameScripts/SessionTutorial.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SessionTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _slides;
+    [SerializeField] private int _mainMenuIndex;
+
+    private int currentSlide = 0;
+
+    private void Start()
+    {
+        ChangeSlide(currentSlide);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            currentSlide++;
+            if (currentSlide <= _slides.Length-1)
+            {
+                ChangeSlide(currentSlide);
+            }
+            else
+            {
+                // Tutorial complete
+                SceneManager.LoadScene(_mainMenuIndex);
+            }
+        }
+    }
+        
+    private void ChangeSlide(int slideIndex)
+    {
+        foreach (GameObject slide in _slides)
+        {
+            slide.SetActive(false);
+        }
+
+        _slides[slideIndex].SetActive(true);
+    }
+}
